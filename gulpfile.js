fbeb99e532ae4acf6253d334bc55b39c71b77865
yoru <yorudevv@gmail>
2026-05-09 15:36:17 -0300

    actualizacon

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,68 @@
+const { dest, watch, series, src } = require("gulp");
 
 // css , sass
-const { dest, watch, series, src } = require("gulp");
-const autoPrefixer = require("gulp-autoprefixer").default;
 const sass = require("gulp-sass")(require("sass"));
-
-
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
 // imagenes
 const imagemin = require("gulp-imagemin").default;
 const webp = require("gulp-webp").default;
-const avif = require('gulp-avif');
-
-
-
-
-// version webp
-function imagenesWebp(done){
-    src("src/img/**/*.{jpg,png}")
-    .pipe(webp())
-    .pipe(dest("build/img"))
-    done();
-}
-
-// version avif
-function imagenesavif(done){
-    src("src/img/**/*.{jpg,png}")
-    .pipe(avif())
-    .pipe(dest("build/img"))
-    done();
-}
-
+const avif = require("gulp-avif");
 
 function css(done) {
   src("src/scss/style.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(autoPrefixer({ autoPrefixer}))
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano()
+    ]))
+    .pipe(sourcemaps.write("."))
     .pipe(dest("build/css"));
 
   done();
 }
 
-function imagenes(done){
-    src("src/img/**/*")
-    .pipe(imagemin({optimizationLevel: 3}))
-    .pipe(dest("build/img"))
-    done();
+// version webp
+function imagenesWebp(done) {
+  src("src/img/**/*.{jpg,png}")
+    .pipe(webp())
+    .pipe(dest("build/img"));
+
+  done();
 }
 
+// version avif
+function imagenesavif(done) {
+  src("src/img/**/*.{jpg,png}")
+    .pipe(avif())
+    .pipe(dest("build/img"));
+
+  done();
+}
+
+function imagenes(done) {
+  src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("build/img"));
+
+  done();
+}
 
 function dev() {
   watch("src/scss/**/*.scss", css);
   watch("src/img/**/*", imagenes);
 }
 
+exports.default = series(
+  imagenes,
+  imagenesWebp,
+  imagenesavif,
+  css,
+  dev
+);
 
-
-
-exports.default = series(imagenes, imagenesWebp, imagenesavif, css, dev);
 exports.css = css;
